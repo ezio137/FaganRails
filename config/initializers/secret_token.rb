28100fbcadf532cb1e03fcb17c35016e82af4fc4
ezio137 <ezio137@ezio137-dell.(none)>
2011-10-25 15:02:54 -0200

    projeto rails criado

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FaganRails::Application.config.secret_token = 'a721688898431b9b9ac3d46cabf5ea23c3b00fde1d31c1145dce856b209eb39b082227c50bd2b93c9b4aa6132e91de62e52720445d674913e4f7d1d1f45f81c0'
